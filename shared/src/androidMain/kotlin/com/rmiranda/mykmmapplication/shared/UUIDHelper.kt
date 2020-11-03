6011df64fe8dcbd4a1dcdfcdb91c4c683f92c293
Rodrigo Miranda <rodrigo.kreloco@gmail.com>
2020-11-03 17:24:37 -0600

    ✨ Add UUIDHelper Android implementation

---
+++ shared/src/androidMain/kotlin/com/rmiranda/mykmmapplication/shared/UUIDHelper.kt
@@ -0,0 +1,11 @@
+package com.rmiranda.mykmmapplication.shared
+
+import java.util.*
+
+/**
+ * Created by rodrigomiranda on 11/3/20.
+ * Applaudo Studios
+ */
+actual class UUIDHelper {
+    actual fun randomUUID() = UUID.randomUUID().toString()
+}
